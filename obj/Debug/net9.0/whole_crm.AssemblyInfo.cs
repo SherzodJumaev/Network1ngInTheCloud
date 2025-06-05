8f5a2d7f397635b8ad746ec8d71c7c5f0aaad8a8
SherzodJumayev <112978420+SherzodJumaev@users.noreply.github.com>
2025-06-05 11:20:41 +0500

    updated

--- obj/Debug/net9.0/whole_crm.AssemblyInfo.cs
+++ obj/Debug/net9.0/whole_crm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("whole_crm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f667a924f2710b8f0abb06e6f17f9715f02dfb6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e99e5e68fefbe3fdae32008f189810ce42a0ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("whole_crm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("whole_crm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
